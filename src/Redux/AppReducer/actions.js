e5a52e1c00b589c04f688a064e761a91f090a429
Rahi999 <101566073+Rahi999@users.noreply.github.com>
2022-07-30 15:22:20 +0530

    Initial commit

---
+++ src/Redux/AppReducer/actions.js
@@ -0,0 +1,24 @@
+import {
+  getProductsFailure,
+  getProductsRequest,
+  getProductsSuccess
+} from "./actionsType";
+
+export const getPRequest = () => {
+  return {
+    type: getProductsRequest
+  };
+};
+
+export const getPFailure = () => {
+  return {
+    type: getProductsFailure
+  };
+};
+
+export const getPsuccess = (payload) => {
+  return {
+    type: getProductsSuccess,
+    payload
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/AppReducer/store";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
